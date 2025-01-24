760d57ad88d14e41684ddf9fc27a7cd086336a9f
SonicRivera <connerrivera@gmail.com>
2025-01-23 23:11:50 -0700

    Working on KingMovesCalculator

---
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -0,0 +1,98 @@
+package chess;
+
+import java.util.ArrayList;
+
+public class KingMovesCalculator extends PieceMovesCalculator {
+
+    private final ChessPosition startPosition;
+    private final ChessPiece piece;
+    private int row;
+    private int col;
+    private ChessBoard Cboard;
+
+    public KingMovesCalculator(ChessBoard board, ChessPosition startPosition, ChessPiece piece) {
+        super(board, startPosition, piece);
+        this.startPosition = startPosition;
+        this.piece = piece;
+        this.row = startPosition.getRow();
+        this.col = startPosition.getColumn();
+        this.Cboard = board;
+
+        if (piece.getPieceType() != ChessPiece.PieceType.KING) {
+            throw new IllegalArgumentException("KingMovesCalculator can only be used with KING pieces.");
+        }
+    }
+
+    public void addMove(ChessPosition start, ChessPosition end, ChessPiece piece, ArrayList<ChessMove> moves){
+        if (!super.Blocked(Cboard, end, this.piece)) {
+            moves.add(new ChessMove(start, end, null));
+        } else {
+            System.out.println("BLOCKED");
+        }
+    }
+
+    public ArrayList<ChessMove> calculateMoves() {
+        Cboard.printBoard();
+
+        ChessPosition start = new ChessPosition(this.row, this.col);
+        ChessPosition end;
+        ArrayList<ChessMove> moves = new ArrayList<>();
+
+        // Check for moving left
+        if (this.col - 1 > 1) {
+            end = new ChessPosition(this.row, this.col - 1);
+            addMove(start, end, piece, moves);
+        }
+
+        // Check for moving up left
+        if (this.col - 1 > 1 && this.row + 1 < 8) {
+            end = new ChessPosition(this.row + 1, this.col - 1);
+            addMove(start, end, piece, moves);
+
+        }
+
+        // Check moves for moving up
+        if (this.row + 1 < 8) {
+            end = new ChessPosition(this.row + 1, this.col);
+            addMove(start, end, piece, moves);
+
+        }
+
+        // Check moves for moving up right
+        if (this.col + 1 < 8 && this.row + 1 < 8) {
+            end = new ChessPosition(this.row + 1, this.col + 1);
+            addMove(start, end, piece, moves);
+
+        }
+
+        // Check for moving right
+        if (this.col + 1 < 8) {
+            end = new ChessPosition(this.row, this.col + 1);
+            addMove(start, end, piece, moves);
+
+        }
+
+        // Check moves for moving down right
+        if (this.col + 1 < 8 && this.row - 1 > 1) {
+            end = new ChessPosition(this.row - 1, this.col + 1);
+            addMove(start, end, piece, moves);
+
+        }
+
+        // Check for moving down
+        if (this.row - 1 > 1) {
+            end = new ChessPosition(this.row - 1, this.col);
+            addMove(start, end, piece, moves);
+
+        }
+
+        // Check moves for moving down left
+        if (this.col - 1 > 1 && this.row - 1 > 1) {
+            end = new ChessPosition(this.row - 1, this.col - 1);
+            addMove(start, end, piece, moves);
+
+        }
+
+        return moves;
+    }
+}
--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -8,10 +8,20 @@ public class PieceMovesCalculator {
     private int row;
     private int col;
     
-    public PieceMovesCalculator(ChessBoard board, ChessPosition startPos){
+    public PieceMovesCalculator(ChessBoard board, ChessPosition startPos, ChessPiece piece){
         this.row = startPos.getRow();
         this.col = startPos.getColumn();
         board.getPiece(startPos);
+    }
+
+    /**
+     * Returns true if the piece is blocked.
+     */
+    public Boolean Blocked(ChessBoard board, ChessPosition endPos, ChessPiece piece){
+        if (board.getPiece(endPos) != null) {
+            return board.getPiece(endPos).getTeamColor() == piece.getTeamColor();
+        }
+        return false;
     }
 
     public ArrayList<ChessMove> calculateMoves(){
