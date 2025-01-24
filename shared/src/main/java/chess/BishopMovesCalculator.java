f6d3c815f2abca1d5d681fdd9e01c05d43d4c837
SonicRivera <connerrivera@gmail.com>
2025-01-24 10:08:07 -0700

    BishopMovesCalculator is now a subclass of PieceMovesCalculator as well as fixed logic

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -3,16 +3,17 @@ package chess;
 import java.sql.Array;
 import java.util.ArrayList;
 
-public class BishopMovesCalculator {
+public class BishopMovesCalculator extends PieceMovesCalculator {
 
 
     private final ChessPosition startPosition;
-    private final ChessPiece.PieceType piece;
+    private final ChessPiece piece;
     private int row;
     private int col;
     private ChessBoard Cboard;
 
-    public BishopMovesCalculator(ChessBoard board, ChessPosition startPosition, ChessPiece.PieceType piece) {
+    public BishopMovesCalculator(ChessBoard board, ChessPosition startPosition, ChessPiece piece) {
+        super(board, startPosition, piece);
         this.startPosition = startPosition;
         this.piece = piece;
         this.row = startPosition.getRow();
@@ -20,28 +21,39 @@ public class BishopMovesCalculator {
         this.Cboard = board;
 
 
-        if (piece != ChessPiece.PieceType.BISHOP){
+        if (this.piece.getPieceType() != ChessPiece.PieceType.BISHOP){
             throw new IllegalArgumentException("BishopMovesCalculator can only be used with BISHOP pieces.");
         }
 
     }
 
+    public void resetStart(){
+        this.row = startPosition.getRow();
+        this.col = startPosition.getColumn();
+    }
+
     public ArrayList<ChessMove> calculateMoves(){
+        Cboard.printBoard();
+
         ChessPosition start = new ChessPosition(this.row, this.col);
         ChessPosition end = new ChessPosition(0, 0);
 
         ArrayList<ChessMove> moves = new ArrayList<>();
        //Check for moving up left
+        resetStart();
        while (true){
            this.row += 1;
            this.col -= 1;
            ChessPosition pos = new ChessPosition(row, col);
-           if (row > 8 || col < 1 || Cboard.getPiece(pos) != null) {
+           if (row > 7 || col < 1){
+               end = new ChessPosition(this.row+1, this.col-1);
+               if (super.Blocked(Cboard, end, this.piece)) {
+                   break;
+               }
                break;
            } else {
             end = new ChessPosition(this.row, this.col);
-            ChessMove move = new ChessMove(start, end, null);
-            moves.add(move);
+            moves.add(new ChessMove(start, end, null));
            }
 
        }
@@ -49,46 +61,58 @@ public class BishopMovesCalculator {
 
 
         //Check moves for moving up right
+        resetStart();
         while (true){
             this.row += 1;
             this.col += 1;
             ChessPosition pos = new ChessPosition(row, col);
-            if (row > 8 || col > 8 || Cboard.getPiece(pos) != null) {
+            if (row > 7 || col > 7){
+                end = new ChessPosition(this.row+1, this.col-1);
+                if (super.Blocked(Cboard, end, this.piece)) {
+                    break;
+                }
                 break;
             } else {
                 end = new ChessPosition(this.row, this.col);
-                ChessMove move = new ChessMove(start, end, null);
-                moves.add(move);
+                moves.add(new ChessMove(start, end, null));
             }
 
         }
 
         //check for moving down left
+        resetStart();
         while (true){
             this.row -= 1;
             this.col -= 1;
             ChessPosition pos = new ChessPosition(row, col);
-            if (row < 1 || col < 1 || Cboard.getPiece(pos) != null) {
+            if (row < 1 || col < 1){
+                end = new ChessPosition(this.row+1, this.col-1);
+                if (super.Blocked(Cboard, end, this.piece)) {
+                    break;
+                }
                 break;
             } else {
                 end = new ChessPosition(this.row, this.col);
-                ChessMove move = new ChessMove(start, end, null);
-                moves.add(move);
+                moves.add(new ChessMove(start, end, null));
             }
 
         }
 
         //check for moving down right
+        resetStart();
         while (true){
             this.row -= 1;
             this.col += 1;
             ChessPosition pos = new ChessPosition(row, col);
-            if (row < 1 ||  col > 8 || Cboard.getPiece(pos) != null) {
+            if (row < 1 ||  col > 7){
+                end = new ChessPosition(this.row+1, this.col-1);
+                if (super.Blocked(Cboard, end, this.piece)) {
+                    break;
+                }
                 break;
             } else {
                 end = new ChessPosition(this.row, this.col);
-                ChessMove move = new ChessMove(start, end, null);
-                moves.add(move);
+                moves.add(new ChessMove(start, end, null));
             }
 
         }
